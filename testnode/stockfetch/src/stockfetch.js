54c3816e8c665cb5c79c32cc6d94a66fb7edf167
John D. Aynedjian <johndavid5@yahoo.com>
2017-06-05 14:00:07 -0400

    stockfetch.js -- comment-out alt. implementations

--- testnode/stockfetch/src/stockfetch.js
+++ testnode/stockfetch/src/stockfetch.js
@@ -24,52 +24,54 @@ Stockfetch.prototype.readTickersFile = function(filename, onError){
 		};
 
 	fs.readFile(filename, processResponse);
-};
+}; /* readTickersFile() */
  
-Stockfetch.prototype.parseTickersJohn = function(rawData){
-
-	rawData = rawData.trim(); // Covers white-space-only situation, and makes for cleaner output...
-
-	if( rawData.length == 0 ){
-		return [];
-	}
-	else {
-		rawArray = rawData.split('\n');
-
-		returnArray = []; 
-
-		rawArray.forEach(function(le_raw_element, le_index, le_array){ 
-			var preened_element = le_raw_element.trim().toUpperCase();	
-
-			// returnArray should only contain elements that 
-			// when trimmed of whitespace and converted to upper case
-			// remain unchanged.
-			if(preened_element == le_raw_element ){
-				returnArray.push( le_raw_element );
-			}
-		});
-
-		return returnArray;
-	}
-
-	/* Note: When the string is empty, split() returns an array containing one empty string,
-	* rather than an empty array. If the string and separator are both empty strings, an empty
-	* array is returned.
-	*/
-};
-
-/* Don't be so smart, Spock, you botched the Acetylcholine test...! */
-/* Nice try, Venkat, but this code allows Blah to get through even
- * though it's not in ALL-CAPS
-*/
-Stockfetch.prototype.parseTickersVenkat = function(rawData){
-	var isInRightFormat = function(str){
-		return str.trim().length !== 0
-			&& str.indexOf(' ') < 0;
-	};
-
-	return rawData.split('\n').filter(isInRightFormat);
-};
+//Stockfetch.prototype.parseTickersJohn = function(rawData){
+//
+//	rawData = rawData.trim(); // Covers white-space-only situation, and makes for cleaner output...
+//
+//	if( rawData.length == 0 ){
+//		return [];
+//	}
+//	else {
+//		/* Note: When the string is empty, split() returns an array containing one empty string,
+//		* rather than an empty array. If the string and separator are both empty strings, an empty
+//		* array is returned.
+//		*/
+//		rawArray = rawData.split('\n');
+//
+//		returnArray = []; 
+//
+//		rawArray.forEach(function(le_raw_element, le_index, le_array){ 
+//			var preened_element = le_raw_element.trim().toUpperCase();	
+//
+//			// returnArray should only contain elements that 
+//			// when trimmed of whitespace and converted to upper case
+//			// remain unchanged.
+//			if(preened_element == le_raw_element ){
+//				returnArray.push( le_raw_element );
+//			}
+//		});
+//
+//		return returnArray;
+//	}
+//
+//}; /* parseTickersJohn() */
+//
+///* MCCOY: Don't be so smart, Spock, you botched the Acetylcholine test...!
+//*
+//* Nice try, Venkat, with your cutesy and succint use of the filter() method,
+//* but this code allows "Blah" to get through even
+//* though it's not in ALL-CAPS
+//*/
+//Stockfetch.prototype.parseTickersVenkat = function(rawData){
+//	var isInRightFormat = function(str){
+//		return str.trim().length !== 0
+//			&& str.indexOf(' ') < 0;
+//	};
+//
+//	return rawData.split('\n').filter(isInRightFormat);
+//}; /* parseTickersVenkat() */
    
 
 Stockfetch.prototype.parseTickersVenkatJohn = function(rawData){
@@ -80,7 +82,7 @@ Stockfetch.prototype.parseTickersVenkatJohn = function(rawData){
 	};
 
 	return rawData.split('\n').filter(isInRightFormat);
-};
+}; /* parseTickersVenkatJohn() */
  
 
 Stockfetch.prototype.parseTickers = Stockfetch.prototype.parseTickersVenkatJohn;
