2f435c4b5c010128586213b051084ca02d4d2c03
John D. Aynedjian <johndavid5@yahoo.com>
2017-06-28 21:26:42 -0400

    Tests for task.all() and task.get()...

---
+++ testexpress/todo/models/task.js
@@ -0,0 +1,14 @@
+var db = require('../db');
+var ObjectId = require('mongodb').ObjectId;
+
+var collectionName = 'tasks';
+
+module.exports = {
+	all: function(callback){
+		db.get().collection(collectionName).find().toArray(callback);
+	},
+
+	get: function(taskId, callback){
+		db.get().collection(collectionName).find({'_id': new ObjectId(taskId)}).limit(1).next(callback);
+	},
+};
---
+++ testexpress/todo/test/server/models/task-test.js
@@ -0,0 +1,121 @@
+var expect = require('chai').expect;
+var db = require('../../../db');
+var ObjectId = require('mongodb').ObjectId;
+var task = require('../../../models/task');
+
+describe('task models tests', function()
+{
+	var sampleTask;
+	var sampleTasks;
+	var debug = 0; 
+
+	// before() and after() with Mocha's watch option: keeps database
+	// connection open only when necessary 
+
+	// before() all tests...
+	before(function(done){
+		// Create a DB connection to 'todotest' before any tests run...
+		db.connect('mongodb://localhost/todotest', done);	
+	});
+
+	// after() all tests...
+	after(function(){
+		// Close the connection at the end of the last test...
+		db.close();
+	});
+
+	// Helper function to creat id of type
+	// ObjectId expected by MOngoDB.
+	var id = function(idValue){
+		return new ObjectId(idValue);
+	};
+
+	// Before each test, create a test fixture of
+	// sample tasks and insert them into the tasks
+	// collection of the database.
+	beforeEach(function(done){
+
+		// Single canned task...use later for testing the add() function...
+		sampleTask = {name: 'a new task', month: 12, day: 10, year: 2016 };
+
+		sampleTasks = [
+			{_id: id('123412341240'), name: 'task1', month: 10, day: 5, year: 2016},	
+			{_id: id('123412341241'), name: 'task2', month: 11, day: 2, year: 2016},	
+			{_id: id('123412341242'), name: 'task3', month: 12, day: 8, year: 2016},	
+			
+		];
+
+		db.get().collection('tasks').insert(sampleTasks, done);
+	}); /* beforeEach() */
+
+	// Wipe out the 'tasks' collection after each test...
+	afterEach(function(done){
+		db.get().collection('tasks').drop(done);
+	});
+
+	it('all() should return all the tasks',
+		function allTest(done){
+
+			var callback = function allTestCallback(err, tasks){
+
+				var sWho ="allTestCallback";
+
+				if( debug ){
+					console.log(sWho + "(): err = ", err );
+					console.log(sWho + "(): tasks = ", tasks );
+				}
+				expect(tasks).to.be.eql(sampleTasks);
+				done();
+			};
+
+			task.all(callback);
+
+		}/* allTest() */
+	); 
+
+	it('get() should return task with a given id',
+		function getTest(done){
+
+			var ourCallback = function getTestCallback(err, task){
+
+				var sWho ="getTestCallback";
+
+				if( debug ){
+					console.log(sWho + "(): err = ", err );
+					console.log(sWho + "(): task = ", task );
+				}
+				expect(task.name).to.be.eql('task1');
+				expect(task.month).to.be.eql(10);
+				expect(task).to.be.eql(sampleTasks[0]);
+				done();
+			};
+
+			task.get('123412341240', ourCallback);
+
+		}/* getTest() */
+	); 
+
+	it('get() should return null for non-existing task...',
+		function getNullTest(done){
+
+			var ourCallback = function getNullCallback(err, task){
+
+				var sWho ="getNullCallback";
+
+				if( debug ){
+					console.log(sWho + "(): err = ", err );
+					console.log(sWho + "(): task = ", task );
+				}
+				//expect(task).to.be.eql(null);
+				expect(task).to.be.null;
+				done();
+			};
+
+			task.get(90210, ourCallback);
+
+		}/* getNullTest() */
+	); 
+
+});
+
+
